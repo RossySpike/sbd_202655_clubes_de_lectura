a7d2be52fd9022de0fa7a2d0c384c60472b789b2
rossyspike <jmcamero.24@est.ucab.edu.ve>
2026-06-06 10:51:14 -0400

    split

--- scripts/proceds-funcs-triggs.sql
+++ scripts/proceds-funcs-triggs.sql
@@ -47,8 +47,8 @@ CREATE OR REPLACE TRIGGER tgr_hora_grupo_ninos
 BEFORE INSERT OR UPDATE ON grupo
 FOR EACH ROW
 WHEN (NEW.tipo_grupo = 'niños')
-BEGIN
-  IF TO_CHAR(:NEW.hora_reunion, 'HH24:MI') > '17:00' THEN
+BEGILN
+  IF TO_CHAR(:NEW.hora_reunion, 'HH24:MI') > '17:00' THEN -- here '>' -> '>='
     RAISE_APPLICATION_ERROR(-20003,
       'Los grupos de niños deben iniciar a más tardar a las 17:00 para terminar antes de las 19:00.');
   END IF;
@@ -69,3 +69,123 @@ BEGIN
   END IF;
 END;
 /
+
+
+-- SPLITS:      Max       Min
+-- ADULTOS      30        10
+-- JOVENES      15        5
+-- NIÑOS        15        10
+-- "no se puede incluir un nuevo integrante o realizar un split mientras se esté discutiendo un libro" PAG 3
+-- "Cuando se realiza esa división o split se dejan en el grupo original los miembros más antiguos y, luego por cada nueva inscripción se van asignando los nuevos miembros de manera equitativa" PAG 3
+CREATE OR REPLACE PROCEDURE sp_split_grupo(
+    p_id_club_original IN NUMBER,
+    p_id_grupo_original IN NUMBER,
+    p_tipo_grupo IN VARCHAR2,
+    p_dia_reunion IN NUMBER,
+    p_hora_reunion IN DATE,
+    p_nuevo_grupo_id OUT NUMBER
+) AS
+    v_min_miem NUMBER;
+BEGIN
+    IF p_tipo_grupo = 'adultos' THEN
+        v_min_miem := 10;
+    ELSIF p_tipo_grupo = 'jovenes' THEN
+        v_min_miem := 5;
+    ELSE  -- niños
+        v_min_miem := 10;
+    END IF;
+    
+    -- Crear nuevo grupo
+    INSERT INTO grupo (id_club, tipo_grupo, fecha_creacion, dia_reunion, hora_reunion)
+    VALUES (p_id_club_original, p_tipo_grupo, SYSDATE, p_dia_reunion, p_hora_reunion)
+    RETURNING id_grupo INTO p_nuevo_grupo_id;
+    
+    -- (traspaso)
+    UPDATE g_lec 
+    SET id_grupo = p_nuevo_grupo_id
+    WHERE id_club =p_id_club_original AND id_grupo = p_id_grupo_original 
+      AND fec_f IS NULL
+      AND id_lector IN (
+          SELECT id_lector FROM (
+              SELECT id_lector, fec_i
+              FROM g_lec
+              WHERE id_club = p_id_club_original AND id_grupo = p_id_grupo_original 
+                AND fec_f IS NULL
+              ORDER BY fec_i DESC  -- Los mas nuevos primero para que se queden en el anterior los mas antiguos
+              FETCH FIRST v_min_miem ROWS ONLY
+          )
+      );
+    
+    COMMIT;
+END;
+/
+
+
+CREATE OR REPLACE TRIGGER tgr_grupo_lleno
+BEFORE INSERT  ON g_lec-- NOTE: fecha_i, fec_i Quiza un trigger que revise que la fecha de ingreso sea >= a la de creacion en grupo
+FOR EACH ROW
+DECLARE
+  f_grupo grupo%ROWTYPE;
+  lector_estatus VARCHAR2(8); -- 'activo' o 'retirado'
+  num_miembros NUMBER(2);
+  max_miem NUMBER(2);
+  v_nuevo_grupo_id NUMBER;
+  v_se_hizo_ultima CHAR(1);
+BEGIN
+
+    BEGIN
+SELECT ultima INTO v_se_hizo_ultima FROM calendario_reunion_mes  WHERE id_club = :NEW.id_club AND id_grupo = :NEW.id_grupo AND realizada = 'S' ORDER BY fecha DESC FETCH FIRST 1 ROWS ONLY;
+
+IF v_se_hizo_ultima != 'S' THEN
+                  RAISE_APPLICATION_ERROR(-20006, 'No se puede agregar miembros mientras se discute un libro');
+
+END IF;
+EXCEPTION
+    WHEN NO_DATA_FOUND THEN
+        NULL; -- No hay reuniones realizadas, se puede agregar
+    END;
+    SELECT * INTO f_grupo 
+    FROM grupo 
+    WHERE id_club = :NEW.id_club AND id_grupo = :NEW.id_grupo ;
+
+
+  BEGIN
+    SELECT estatus INTO lector_estatus FROM historia_membresia WHERE id_club = :NEW.id_club AND id_lector = :NEW.id_lector AND estatus = 'activo';
+  EXCEPTION
+  WHEN NO_DATA_FOUND THEN
+                  RAISE_APPLICATION_ERROR(-20007, 'El lector no tiene una membresia activa en el club asociado al grupo.');
+  END;
+  SELECT COUNT(*) INTO num_miembros FROM g_lec gl WHERE gl.id_club = :NEW.id_club AND gl.id_grupo = :NEW.id_grupo AND gl.fec_f IS NULL;
+    -- Validar de grupo
+    IF f_grupo.tipo_grupo = 'adultos' THEN
+      max_miem := 30;
+    END IF;
+    IF f_grupo.tipo_grupo = 'jovenes' THEN
+      max_miem := 15;
+    END IF;
+    IF f_grupo.tipo_grupo = 'niños' THEN
+      max_miem := 15;
+    END IF;
+    IF num_miembros >= max_miem THEN
+         sp_split_grupo(
+            p_id_club_original => f_grupo.id_club,
+            p_id_grupo_original => f_grupo.id_grupo,
+            p_tipo_grupo => f_grupo.tipo_grupo,
+            p_dia_reunion => f_grupo.dia_reunion,
+            p_hora_reunion => f_grupo.hora_reunion,
+            p_nuevo_grupo_id => v_nuevo_grupo_id
+        );
+:NEW.id_grupo := v_nuevo_grupo_id;
+
+  END IF;
+END;
+/
+
+-- Trigger para que cuando se retire un miembro se quite de g_lec
+-- Trigger para  validar que un moderador sea valido
+-- Trigger para validar "Los lectores de un club pueden ser adultos (>a 25 años), niños (entre 6 y 12 años) y jóvenes (entre 13 y 25 años). No se mezclan los grupos aunque un mismo club puede agrupar los tres tipos" pag 3
+-- Trigger Un mismo moderador no está simultáneamente asignado a más de un grupo – cuando terminen las reuniones de discusión del libro que está moderando entonces la persona estaría disponible para otra moderación.
+-- Trigger Un integrante puede mudarse de ciudad o país y unirse a otro club o simplemente cambiar de club en su misma ciudad (no se aceptan nuevos miembros que tengan deudas pendientes en el club anterior) 
+-- Trigger un miembro no puede estar activo en más de un club a la vez y en cada uno pertenece a un solo grupo de lectura simultáneamente.
+-- Trigger la discusión puede durar de 1 a 3 reuniones máximo dependiendo del tamaño y complejidad de la obra.
+-- Trigger si un integrante deja de asistir a mas del 30% de los encuentros en un bimestre es retirado del club y no se puede volver a unir al mismo
